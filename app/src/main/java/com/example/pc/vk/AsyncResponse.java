0613f14da979ab84b5ac17604dc1c675adc757c1
Evgeny Orlov <evgenyorlov1@gmail.com>
2015-09-25 04:04:13 +0300

    Async

---
+++ app/src/main/java/com/example/pc/vk/AsyncResponse.java
@@ -0,0 +1,5 @@
+package com.example.pc.vk;
+
+public interface AsyncResponse {
+    void processFinish(Object output);
+}
--- app/src/main/java/com/example/pc/vk/MainActivity.java
+++ app/src/main/java/com/example/pc/vk/MainActivity.java
@@ -1,52 +1,75 @@
 package com.example.pc.vk;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.content.Intent;
-import android.content.Context;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends Activity {
 
-    private MyBroadcastReceiver myBroadcastReceiver;
-
+    private String result = "";
+    TextView form;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        form = (TextView) findViewById(R.id.text);
+        try {
+            MyAsyncTask mt = new MyAsyncTask(new AsyncResponse() {
+                @Override
+                public void processFinish(Object output) {
+                    result = output.toString();
+                }
+            });
+            mt.execute();
+        } catch(Exception e) {form.setText(e.toString());}
 
-        Intent intentService = new Intent(MainActivity.this, MyIntentService.class);
-        myBroadcastReceiver = new MyBroadcastReceiver();
-
-        IntentFilter intentFilter = new IntentFilter(MyIntentService.ACTION_MYINTENTSERVICE);
-        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
-        registerReceiver(myBroadcastReceiver, intentFilter);
-        Toast.makeText(getApplicationContext(), "onCreate end", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(myBroadcastReceiver);
-        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
-    }
 
-    public class MyBroadcastReceiver extends BroadcastReceiver {
 
-        public static final String PROCESS_RESPONSE = "com.example.pc.vk.intent.action.PROCESS_RESPONSE";
+    private class MyAsyncTask extends AsyncTask<Object, Object, Object> {
+
+        public AsyncResponse delegate = null;//Call back interface
+
+        public MyAsyncTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;//Assigning call back interfacethrough constructor
+        }
 
         @Override
-        public void onReceive(Context context, Intent intent) {
-            String result  = intent.getStringExtra("Response");
-            TextView form = (TextView) findViewById(R.id.text);
-            Toast.makeText(getApplicationContext(), "Title", Toast.LENGTH_SHORT).show();
-            form.setText(result);
+        protected Object doInBackground(Object... params) {
+
+            StringBuilder builder = new StringBuilder();
+            try {
+                URL url_point = new URL("https://api.vk.com/method/wall.get?owner_id=-27460410&count=5&v=5.37");
+                HttpURLConnection urlConnection = (HttpURLConnection) url_point.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoInput(true);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.connect();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String line = "";
+                builder.append("test");
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+            } catch (Exception e) {}
+            return builder;
         }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            delegate.processFinish(result);
+        }
+
     }
 }
 
